02adee4cb0dcdcd5e593e77a4d30c426fb0cc230
jfobrien29 <thejackobrien29@gmail.com>
2022-01-05 18:24:39 -0500

    add google Analytics to personal site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import '@/styles/global.css';
+import { useRouter } from 'next/router';
+
+// log the pageview with their URL
+export const pageview = (url: string) => {
+  // @ts-ignore
+  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+    page_path: url,
+  });
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
