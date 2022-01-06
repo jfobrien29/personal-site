c1d05b13c087ce551df305a3f9870e540de6011c
jfobrien29 <thejackobrien29@gmail.com>
2022-01-05 19:40:13 -0500

    build lint fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,11 @@ import { useRouter } from 'next/router';
 
 // log the pageview with their URL
 export const pageview = (url: string) => {
-  // @ts-ignore
-  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-    page_path: url,
-  });
+  if (typeof window !== 'undefined') {
+    (window as any).gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+      page_path: url,
+    });
+  }
 };
 
 export default function MyApp({ Component, pageProps }: AppProps) {
