6dbb1cc59bf43a13a63b0e561847c63ea5072d86
jfobrien29 <thejackobrien29@gmail.com>
2022-01-07 14:59:03 -0500

    Add amazon links to all books

--- src/components/books/data.ts
+++ src/components/books/data.ts
@@ -98,6 +98,10 @@ export const LIFT_OFF = {
   title: `Lift Off: Elon Musk and the Desperate Early Days That Launched SpaceX`,
   author: 'Eric Berger',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  links: {
+    book:
+      'https://www.amazon.com/Liftoff-Desperate-Early-Launched-SpaceX-ebook/dp/B088FQK2K2/',
+  },
   quickDescription:
     'Early stage startup meets rocket science. A wild story of ambitious and talented people.',
 };
@@ -107,6 +111,10 @@ export const THE_NEW_NEW_THING = {
   title: `The New New Thing: A Silicon Valley Story`,
   author: 'Michael Lewis',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  links: {
+    book:
+      'https://www.amazon.com/New-Thing-Silicon-Valley-Story-ebook/dp/B000RH0CA4/',
+  },
   quickDescription:
     'The beginnings of Silicon Valley, the internet, Netscape, and the legendary Jim Clark.',
 };
@@ -116,8 +124,12 @@ export const SHOE_DOG = {
   title: `Shoe Dog: A Memoir by the Creator of Nike`,
   author: 'Phil Knight',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  links: {
+    book:
+      'https://www.amazon.com/Shoe-Dog-Memoir-Creator-Nike-ebook/dp/B0176M1A44/',
+  },
   quickDescription:
-    "Building Nike was a grind with no obvious path forward, lot's of luck, lot's of grit, and a ton of trips to Japan.",
+    "Building Nike was a grind with no obvious path forward. Lot's of luck, grit, and trips to Japan.",
 };
 
 export const BAD_BLOOD = {
@@ -125,7 +137,12 @@ export const BAD_BLOOD = {
   title: `Bad Blood: Secrets and Lies in a Silicon Valley Startup`,
   author: 'John Carreyrou',
   category: CATEGORY.ENTREPRENEUR_STORY,
-  quickDescription: 'Elizabeth Holmes is facinating. How NOT to run a startup.',
+  links: {
+    book:
+      'https://www.amazon.com/Bad-Blood-Secrets-Silicon-Startup-ebook/dp/B078VW3VM7/',
+  },
+  quickDescription:
+    "Elizabeth Holmes is a die hard entrepreneur and one of the most persuasive leaders I've read about. How NOT to run a startup.",
 };
 
 // Software and Product
@@ -135,6 +152,10 @@ export const UNICORN_PROJECT = {
   title: `The Unicorn Project: A Novel about Digital Disruption, Redshirts, and Overthrowing the Ancient Powerful Order`,
   author: 'Gene Kim',
   category: CATEGORY.SOFTWARE,
+  links: {
+    book:
+      'https://www.amazon.com/Unicorn-Project-Developers-Disruption-Thriving-ebook/dp/B07QT9QR41',
+  },
   quickDescription:
     'Put into the form of a novel, an informative story about dev ops, team structure, and agile developement practices for software companies.',
 };
@@ -143,6 +164,10 @@ export const TECHINICALLY_WRONG = {
   title: `Techinically Wrong: Sexist Apps, Biased Algorithms, and Other Threats of Toxic Tech`,
   author: 'Sara Wachter-Boettcher',
   category: CATEGORY.SOFTWARE,
+  links: {
+    book:
+      'https://www.amazon.com/Technically-Wrong-Sexist-Algorithms-Threats-ebook/dp/B06XJBGPT9/',
+  },
   quickDescription:
     'We take tech generally as truth (ex. Google Search). Eye openeing overview of biases baked into tech products and why diversity is a necessity.',
 };
@@ -151,6 +176,10 @@ export const CLEAN_CODE = {
   title: `Clean Code: A Handbook of Agile Software Craftsmanship`,
   author: 'Robert C. Martin',
   category: CATEGORY.SOFTWARE,
+  links: {
+    book:
+      'https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship-ebook/dp/B001GSTOAM/',
+  },
   quickDescription:
     'A book about software craftsmanship and best practices to write good, clean code.',
 };
@@ -159,6 +188,10 @@ export const DESIGN_OF_EVERYDAY_THINGS = {
   title: `Design of Everyday Things`,
   author: 'Donald A. Norman',
   category: CATEGORY.SOFTWARE,
+  links: {
+    book:
+      'https://www.amazon.com/Design-Everyday-Things-Revised-Expanded-ebook/dp/B00E257T6C/',
+  },
   quickDescription: 'Product design from the lense of things you use everyday.',
 };
 export const HOOKED = {
@@ -166,6 +199,10 @@ export const HOOKED = {
   title: `Hooked: How to Build Habit-Forming Products`,
   author: 'Nir Eyal',
   category: CATEGORY.SOFTWARE,
+  links: {
+    book:
+      'https://www.amazon.com/Hooked-How-Build-Habit-Forming-Products-ebook/dp/B00LMGLXTS/',
+  },
   quickDescription:
     'Create a trigger, enable some action with variable reward, and built user investment. Soon enough, users are hooked.',
 };
@@ -177,6 +214,10 @@ export const ZERO_TO_ONE = {
   title: `Zero to One: Notes on Startups, or How to Build the Future`,
   author: 'Peter Thiel',
   category: CATEGORY.STARTUP,
+  links: {
+    book:
+      'https://www.amazon.com/Zero-One-Notes-Startups-Future-ebook/dp/B00J6YBOFQ/',
+  },
   quickDescription:
     'Must read startup book. Quick read, foundational, just read it.',
 };
@@ -185,6 +226,10 @@ export const LEAN_STARTUP = {
   title: `The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses`,
   author: 'Eric Ries',
   category: CATEGORY.STARTUP,
+  links: {
+    book:
+      'https://www.amazon.com/Lean-Startup-Entrepreneurs-Continuous-Innovation-ebook/dp/B004J4XGN6/',
+  },
   quickDescription:
     'The mission of a startup is to develop hypotheses and test them quickly. A framework for setting clear goals, measuring them, and deciding what to do next.',
 };
@@ -193,6 +238,10 @@ export const ART_OF_THE_START = {
   title: `The Art of the Start 2.0: The Time-Tested, Battle Hardened Guide for Anyone Starting Anything`,
   author: 'Guy Kawasaki',
   category: CATEGORY.STARTUP,
+  links: {
+    book:
+      'https://www.amazon.com/Art-Start-2-0-Time-Tested-Battle-Hardened-ebook/dp/B00MNNAOX0/',
+  },
   quickDescription:
     'How to start an internet business from an early Apple employee and multi-time startup founder.',
 };
@@ -201,6 +250,10 @@ export const HARD_THING_ABOUT_HARD_THINGS = {
   title: `The Hard Thing About Hard Things: Building a Business When There Are No Easy Answers`,
   author: 'Ben Horowitz',
   category: CATEGORY.STARTUP,
+  links: {
+    book:
+      'https://www.amazon.com/Hard-Thing-About-Things-Building-ebook/dp/B00DQ845EA/',
+  },
   quickDescription:
     'Difficult decisions and hard situations that WILL happen during company building. Told by Ben Horowitz, Opsware founder and a16z GP.',
 };
@@ -212,6 +265,10 @@ export const EMPTY_PLANET = {
   title: 'Empty Planet: The Shock of Global Population Decline',
   author: 'Darrell Bricker & John Ibbitson',
   category: CATEGORY.OTHER,
+  links: {
+    book:
+      'https://www.amazon.com/Empty-Planet-Global-Population-Decline-ebook/dp/B07CWHYVW5/',
+  },
   quickDescription:
     'Human population will soon plummet. This is a big problem with no easy answers.',
 };
@@ -222,6 +279,10 @@ export const DEFINING_DECADE = {
     'The Defining Decade: Why Your Twenties Matter—And How to Make the Most of Them Now',
   author: 'Meg Jay',
   category: CATEGORY.OTHER,
+  links: {
+    book:
+      'https://www.amazon.com/Defining-Decade-Your-Twenties-Matter-ebook/dp/B005SCSCAU/',
+  },
   quickDescription:
     'High level advice and stories on work, relationships, and your physical health for people in their 20s.',
 };
