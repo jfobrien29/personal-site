a3c907e65f00713be2ee52e5df6f9a6ddbae8f58
jfobrien29 <thejackobrien29@gmail.com>
2022-01-07 10:58:38 -0500

    add book quick descriptions

--- src/components/books/Books.tsx
+++ src/components/books/Books.tsx
@@ -9,7 +9,9 @@ const BookListing: React.FC<{ book: IBook }> = ({ book }) => {
       <a>
         <h2 className="mt-8 text-xl font-semibold">{book.title}</h2>
         <p className="mt-1 text-md italic">By {book.author}</p>
-        {/* <p className="mt-2 text-md elipsis-2-lines-text">{blog.excerpt}</p> */}
+        <p className="mt-1 text-md elipsis-2-lines-text">
+          {book.quickDescription}
+        </p>
       </a>
     </Link>
   );
@@ -37,11 +39,14 @@ const BookSection: React.FC<{ category: CATEGORY; header: string }> = ({
 const Books: React.FC = () => (
   <BaseLayout
     title="Jack O'Brien's Recommended Books"
-    description="Books I've read and recommend! Includes my must reads, entreupreneur stories, tech books, and other categories."
+    description="Books I've read and recommend! Includes my must reads, entreupreneur stories, software books, and other categories."
   >
-    <div className="flex flex-col max-w-3xl px-4">
+    <div className="flex flex-col max-w-2xl px-4">
       <h1 className=" text-white text-3xl font-semibold">Books</h1>
-      <div className="mt-1">Books I've read and recommend you should too!</div>
+      <div className="mt-1">
+        Books I've read and recommend you should too. If you find something
+        interesting on this list, let me know!
+      </div>
 
       <BookSection
         category={CATEGORY.MUST_READ}
--- src/components/books/data.ts
+++ src/components/books/data.ts
@@ -20,6 +20,7 @@ export interface IBook {
   imageUrl?: string;
   category: CATEGORY;
   review?: string;
+  quickDescription: string;
 }
 
 // Must Reads
@@ -29,6 +30,8 @@ export const LIFESPAN = {
   title: 'Lifespan: Why We Age - and Why We Don’t Have To',
   author: 'Dr. David Sinclair',
   category: CATEGORY.MUST_READ,
+  quickDescription:
+    "Longevity and healthspan will meaningfully increase in our lifetimes. We've figured out the mechanism of aging, and every technical problem has a technical solution.",
 };
 
 export const WIRES_OF_WAR = {
@@ -36,6 +39,8 @@ export const WIRES_OF_WAR = {
   title: 'Wires of War: Technology and the Global Struggle for Power and Order',
   author: 'Jacob Helberg',
   category: CATEGORY.MUST_READ,
+  quickDescription:
+    'China is openly attempting to take over global internet content, infrasturcture, and content. An authoritarian government in charge of the internet threatens democracy everywhere.',
 };
 
 export const CODE_BREAKER = {
@@ -44,6 +49,8 @@ export const CODE_BREAKER = {
     'Code Breaker: Jennifer Doudna, Gene Editing, and the Future of the Human Race',
   author: 'Walter Isaacson',
   category: CATEGORY.MUST_READ,
+  quickDescription:
+    "CRISPR genome editing was discovered in 2012, and the first gene edited human was born in 2018. Story of CRISPR discovery, how it's going to be used, and the moral delimas of cheap, easy gene editing.",
 };
 
 export const CHASING_THE_SCREAM = {
@@ -51,6 +58,8 @@ export const CHASING_THE_SCREAM = {
   title: 'Chasing the Scream: The First and Last Days of the War on Drugs',
   author: 'Johann Hari',
   category: CATEGORY.MUST_READ,
+  quickDescription:
+    "Wild. Outdated global drug policy doesn't work, creates a dangerous world for everyone remotely involved, and the war on drugs is on it's way out.",
 };
 
 export const HOW_TO_AVOID_A_CLIMATE_DISASTER = {
@@ -58,6 +67,8 @@ export const HOW_TO_AVOID_A_CLIMATE_DISASTER = {
   title: `How to Avoid a Climate Disaster: Solutions We Have and the Breakthroughs We’ll Need`,
   author: 'Bill Gates',
   category: CATEGORY.MUST_READ,
+  quickDescription:
+    "We've got until 2030 to get on a direct path forward, and 2050 to reach net 0 carbon emissions. It's possible with focus, collaboration, and innovation.",
 };
 
 // Entrepreneur stories
@@ -67,6 +78,8 @@ export const LIFT_OFF = {
   title: `Lift Off: Elon Musk and the Desperate Early Days That Launched SpaceX`,
   author: 'Eric Berger',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  quickDescription:
+    'Early stage startup meets rocket science. A wild story of ambitious and talented people.',
 };
 
 export const THE_NEW_NEW_THING = {
@@ -74,6 +87,8 @@ export const THE_NEW_NEW_THING = {
   title: `The New New Thing: A Silicon Valley Story`,
   author: 'Michael Lewis',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  quickDescription:
+    'The beginnings of Silicon Valley, the internet, Netscape, and the legendary Jim Clark.',
 };
 
 export const SHOE_DOG = {
@@ -81,6 +96,8 @@ export const SHOE_DOG = {
   title: `Shoe Dog: A Memoir by the Creator of Nike`,
   author: 'Phil Knight',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  quickDescription:
+    "Building Nike was a grind with no obvious path forward, lot's of luck, lot's of grit, and a ton of trips to Japan.",
 };
 
 export const BAD_BLOOD = {
@@ -88,6 +105,7 @@ export const BAD_BLOOD = {
   title: `Bad Blood: Secrets and Lies in a Silicon Valley Startup`,
   author: 'John Carreyrou',
   category: CATEGORY.ENTREPRENEUR_STORY,
+  quickDescription: 'Elizabeth Holmes is facinating. How NOT to run a startup.',
 };
 
 // Software and Product
@@ -97,30 +115,39 @@ export const UNICORN_PROJECT = {
   title: `The Unicorn Project: A Novel about Digital Disruption, Redshirts, and Overthrowing the Ancient Powerful Order`,
   author: 'Gene Kim',
   category: CATEGORY.SOFTWARE,
+  quickDescription:
+    'Put into the form of a novel, an informative story about dev ops, team structure, and agile developement practices for software companies.',
 };
 export const TECHINICALLY_WRONG = {
   slug: 'techinically-wrong',
   title: `Techinically Wrong: Sexist Apps, Biased Algorithms, and Other Threats of Toxic Tech`,
   author: 'Sara Wachter-Boettcher',
   category: CATEGORY.SOFTWARE,
+  quickDescription:
+    'We take tech generally as truth (ex. Google Search). Eye openeing overview of biases baked into tech products and why diversity is a necessity.',
 };
 export const CLEAN_CODE = {
   slug: 'clean-code',
   title: `Clean Code: A Handbook of Agile Software Craftsmanship`,
   author: 'Robert C. Martin',
   category: CATEGORY.SOFTWARE,
+  quickDescription:
+    'A book about software craftsmanship and best practices to write good, clean code.',
 };
 export const DESIGN_OF_EVERYDAY_THINGS = {
   slug: 'design-of-everyday-things',
   title: `Design of Everyday Things`,
   author: 'Donald A. Norman',
   category: CATEGORY.SOFTWARE,
+  quickDescription: 'Product design from the lense of things you use everyday.',
 };
 export const HOOKED = {
   slug: 'hooked',
   title: `Hooked: How to Build Habit-Forming Products`,
   author: 'Nir Eyal',
   category: CATEGORY.SOFTWARE,
+  quickDescription:
+    'Create a trigger, enable some action with variable reward, and built user investment. Soon enough, users are hooked.',
 };
 
 // Startup Books
@@ -130,24 +157,32 @@ export const ZERO_TO_ONE = {
   title: `Zero to One: Notes on Startups, or How to Build the Future`,
   author: 'Peter Thiel',
   category: CATEGORY.STARTUP,
+  quickDescription:
+    'Must read startup book. Quick read, foundational, just read it.',
 };
 export const LEAN_STARTUP = {
   slug: 'the-lean-startup',
   title: `The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses`,
   author: 'Eric Ries',
   category: CATEGORY.STARTUP,
+  quickDescription:
+    'The mission of a startup is to develop hypotheses and test them quickly. A framework for setting clear goals, measuring them, and deciding what to do next.',
 };
 export const ART_OF_THE_START = {
   slug: 'art-of-the-start',
   title: `The Art of the Start 2.0: The Time-Tested, Battle Hardened Guide for Anyone Starting Anything`,
   author: 'Guy Kawasaki',
   category: CATEGORY.STARTUP,
+  quickDescription:
+    'How to start an internet business from an early Apple employee and multi-time startup founder.',
 };
 export const HARD_THING_ABOUT_HARD_THINGS = {
   slug: 'the-hard-thing-about-hard-things',
   title: `The Hard Thing About Hard Things: Building a Business When There Are No Easy Answers`,
   author: 'Ben Horowitz',
   category: CATEGORY.STARTUP,
+  quickDescription:
+    'Difficult decisions and hard situations that WILL happen during company building. Told by Ben Horowitz, Opsware founder and a16z GP.',
 };
 
 // Other
@@ -157,6 +192,8 @@ export const EMPTY_PLANET = {
   title: 'Empty Planet: The Shock of Global Population Decline',
   author: 'Darrell Bricker & John Ibbitson',
   category: CATEGORY.OTHER,
+  quickDescription:
+    'Human population will soon plummet. This is a big problem with no easy answers.',
 };
 
 export const DEFINING_DECADE = {
@@ -165,6 +202,8 @@ export const DEFINING_DECADE = {
     'The Defining Decade: Why Your Twenties Matter—And How to Make the Most of Them Now',
   author: 'Meg Jay',
   category: CATEGORY.OTHER,
+  quickDescription:
+    'High level advice and stories on work, relationships, and your physical health for people in their 20s.',
 };
 
 export const ALL_BOOKS: IBook[] = [
